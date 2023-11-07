9b07b7590c7c5d40b049ceb2ffc131f587df4a67
Chidambararaja B <chidb@cisco.com>
2023-11-07 20:58:30 +0530

    validator

--- validator.py
+++ validator.py
@@ -6,3 +6,5 @@ import re
 def validate_email(email):
     if len(email) > 7:
         return bool(re.match("^.+@(\[?)[a-zA-Z0-9-.]+.([a-zA-Z]{2,3}|[0-9]{1,3})(]?)$", email))
+
+print(validate_email("chid@mail.com"))
